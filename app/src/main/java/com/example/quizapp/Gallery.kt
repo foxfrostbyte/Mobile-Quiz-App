6475001d2cde517532dc7f31ef5cbc262b2000e6
Elena N.J. <elena.nicole.jacobsen@hotmail.com>
2026-02-02 16:04:59 +0100

    Gallery update
    Added images of some animals + names mapped to each of them. Implemented a lazy-grid gallery and tried to style it a bit.

--- app/src/main/java/com/example/quizapp/Gallery.kt
+++ app/src/main/java/com/example/quizapp/Gallery.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*;
+import androidx.compose.foundation.lazy.grid.*;
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quizapp.ui.theme.QuizAppTheme
 
 class Gallery : ComponentActivity() {
@@ -19,29 +28,95 @@ class Gallery : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuizAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    GalleryScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun GalleryScreen() {
+
+    // Getting list of predefined images + mapping answers:
+    val imageList = listOf(
+        R.drawable.cat_image to "Cat",
+        R.drawable.dog_image to "Dog",
+        R.drawable.fish_image to "Fish",
+        R.drawable.racoon_image to "Racoon",
+        R.drawable.giraffe_image to "Giraffe",
+        R.drawable.tiger_image to "Tiger"
     )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFe5deef))
+    ) {
+        // Vertical stack of main content:
+        Column() {
+
+            // Title of page:
+            Text(
+                text="GALLERY",
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 100.dp, bottom = 10.dp)
+            )
+
+            // Descriptive under-title:
+            Text(
+                text="Add/edit/delete images for quiz",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 15.dp)
+            )
+
+            // Actual grid with the images:
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(imageList) { (imageId, answer) ->
+                    Column() {
+                        // Image card:
+                        Image(
+                            painter = painterResource(id = imageId),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .aspectRatio(1f)
+                                .border(5.dp, Color.White)
+                        )
+                        // Answer text:
+                        Text(
+                            text = answer,
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color.White)
+                                .padding(bottom = 5.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GalleryScreenPreview() {
     QuizAppTheme {
-        Greeting("Android")
+        GalleryScreen()
     }
 }
